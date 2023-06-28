a07f927080a024d384630f4bc9ca0a6b5baafafb
lingfan009 <fan.ling@autra.tech>
2023-06-28 14:19:30 +0800

    [openscene] extract feature from ovseg

--- scripts/feature_fusion/fusion_util.py
+++ scripts/feature_fusion/fusion_util.py
@@ -67,6 +67,34 @@ def extract_openseg_img_feature(img_dir, openseg_model, text_emb, img_size=None,
 
     return feat_2d
 
+def extract_ovseg_img_feature(img_dir, openseg_model, text_emb, img_size=None, regional_pool=True):
+    '''Extract per-pixel OvSeg features.'''
+
+    # load RGB image
+    np_image_string = read_bytes(img_dir)
+    # run OpenSeg
+    results = openseg_model.signatures['serving_default'](
+            inp_image_bytes=tf.convert_to_tensor(np_image_string),
+            inp_text_emb=text_emb)
+    img_info = results['image_info']
+    crop_sz = [
+        int(img_info[0, 0] * img_info[2, 0]),
+        int(img_info[0, 1] * img_info[2, 1])
+    ]
+    if regional_pool:
+        image_embedding_feat = results['ppixel_ave_feat'][:, :crop_sz[0], :crop_sz[1]]
+    else:
+        image_embedding_feat = results['image_embedding_feat'][:, :crop_sz[0], :crop_sz[1]]
+    if img_size is not None:
+        feat_2d = tf.cast(tf.image.resize_nearest_neighbor(
+            image_embedding_feat, img_size, align_corners=True)[0], dtype=tf.float16).numpy()
+    else:
+        feat_2d = tf.cast(image_embedding_feat[[0]], dtype=tf.float16).numpy()
+
+    feat_2d = torch.from_numpy(feat_2d).permute(2, 0, 1)
+
+    return feat_2d
+
 def save_fused_feature(feat_bank, point_ids, n_points, out_dir, scene_id, args):
     '''Save features.'''
 
